566170cd1c367794c01f99eba17d3a2ccd8bcbfc
Christopher Schmittling <schmittlingc@wustl.edu>
2022-04-16 22:37:59 -0500

    Changes made to the style of the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,6 @@ function Contact() {
     <div id="contact" className="contact-section">
       <div className="contact-content">
         <h3 className="contact-title">Let's Get In Touch</h3>
-        <p className="contact-para">
-          Please fill out the form below and if you would like to add some
-          details about your project, you may do so in the message box at the
-          bottom. I will be in touch as soon as possible.
-        </p>
         <ContactForm />
       </div>
     </div>
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,34 +3,30 @@ import Button from "@mui/material/Button";
 function ContactForm() {
   return (
     <form>
-      <label for="fName">
-        First Name <span>*</span>
-      </label>
-      <input type="text" name="fName" required />
-      <br />
-      <label for="lName">
-        Last Name <span>*</span>
-      </label>
-      <input type="text" name="lName" required />
-      <br />
-      <label for="email">
-        Email <span>*</span>
-      </label>
-      <input type="email" name="email" required />
-      <br />
-      <label for="textarea">Message</label>
-      <textarea type="textarea" rows="4" cols="40" name="textarea" />
-      <br />
+      <div className="row-1">
+        <input type="text" name="fName" placeholder="First Name *" required />
+        <input type="text" name="lName" placeholder="Last Name *" required />
+      </div>
+      <div className="row-2">
+        <input type="email" name="email" placeholder="Email *" required />
+        <input type="tel" name="phone" placeholder="Phone" />
+      </div>
+      <textarea
+        type="textarea"
+        name="message"
+        placeholder="Please provide some information about your project here"
+      />
       <Button
         variant="outlined"
         type="submit"
+        className="submitButton"
         sx={{
           padding: "5px 40px",
           backgroundColor: "#00ADB5",
           color: "#eeeeee",
           fontWeight: 500,
           fontSize: "1.1rem",
-          marginBottom: "20px",
+          margin: "5px",
         }}
       >
         Submit
